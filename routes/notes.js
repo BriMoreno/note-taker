2531bb235fa33d1249d7ed68865c61c868254a95
BriMoreno <BriMoreno232@gmail.com>
2022-09-28 19:54:25 -0700

    debugging

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,28 @@
 const route = require('express').Router();
-const { notes } = require('../db/db.json');
-const {makeNote, deleteNote} = require('../helpers/modifynote');
-const {v4:uuidv4} = require('uuid');
+const storeNotes = require('../db/storeNotes');
 
 //route to get notes
 route.get('/notes', (req, res) => {
-    let saved = notes;
-    res.json(saved);
+    storeNotes
+        .fetchNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
 });
 
 //route for new notes
 route.post('/notes', (req, res) => {
-    req.body.id = uuid4();
-    res.join(makeNote(req.body, notes));
-})
+    storeNotes
+        .adding(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err))
+});
 
 //delete note by id
 route.delete('/notes/:id', (req, res) => {
-    const para = req.params.id;
-    deleteNote(para, notes);
-    res.redirect("")
-})
+    storeNotes
+        .subtracting(req.params.id)
+        .then(() => res.json({ ok: true}))
+        .catch(err => res.status(500).json(err))
+});
 
 module.exports = route;
