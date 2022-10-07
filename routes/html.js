d7cf34ff5047235f27092cd230f62cb95945c0b3
BriMoreno <BriMoreno232@gmail.com>
2022-10-07 15:18:26 -0700

    debugging since the notes don't display but they are added to the db

--- routes/html.js
+++ routes/html.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 //route for notes page
 module.exports = function (app) {
+    //get to note page
     app.get('/notes', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/notes.html'))
     });
-
-
+    //show home page
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
--- routes/notes.js
+++ routes/notes.js
@@ -6,9 +6,7 @@ module.exports = function (app) {
     app.get("/notes", (req, res) => {
 
         // to read database
-        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        console.log("\nGET req - Returning notes data: " + JSON.stringify(data));
-        
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); 
         // Sends data to res
         res.json(data);
     });
@@ -18,7 +16,7 @@ module.exports = function (app) {
 
         // makes a new note to display 
         const newNote = req.body;
-        console.log("\n\nPOST req - New Note : " + JSON.stringify(newNote));
+     
 
         // Assigns note a unique id 
         newNote.id = uuidv4();
@@ -27,7 +25,6 @@ module.exports = function (app) {
         // Pushes new note into db
         data.push(newNote);
         fs.writeFileSync('./db/db.json', JSON.stringify(data));
-        console.log("\nSuccessfully added new note to 'db.json' file!");
         res.json(data);
     });
 
@@ -35,12 +32,11 @@ module.exports = function (app) {
 
         // delete by id
         let noteId = req.params.id.toString();
-        console.log(`\n\nDELETE note req for noteId: ${noteId}`);
+      
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
         const newData = data.filter( note => note.id.toString() !== noteId );
         fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-        console.log(`\nSuccessfully deleted note with id : ${noteId}`);
 
         res.json(newData);
     });
